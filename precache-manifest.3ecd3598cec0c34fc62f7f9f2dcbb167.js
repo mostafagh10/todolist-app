6f575cfd22e022ff5c908d12624e0fe0d13b50b3
mostafa ghazaly <ghazalymostafa07@gmail.com>
2020-08-31 02:23:04 +0200

    Updates

---
+++ precache-manifest.3ecd3598cec0c34fc62f7f9f2dcbb167.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf655b17c19fdb0915eb35935e0baf2c",
+    "url": "/todolist-app/index.html"
+  },
+  {
+    "revision": "5bcd359f94cafd999196",
+    "url": "/todolist-app/static/css/2.0cee8fd6.chunk.css"
+  },
+  {
+    "revision": "97e399e681a0e5920746",
+    "url": "/todolist-app/static/css/main.42327fc0.chunk.css"
+  },
+  {
+    "revision": "5bcd359f94cafd999196",
+    "url": "/todolist-app/static/js/2.81ea2c76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-app/static/js/2.81ea2c76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97e399e681a0e5920746",
+    "url": "/todolist-app/static/js/main.43cff158.chunk.js"
+  },
+  {
+    "revision": "fe40b92f13d336724508",
+    "url": "/todolist-app/static/js/runtime-main.742dd6c3.js"
+  }
+]);
